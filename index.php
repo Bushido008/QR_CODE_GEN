dcade8ab0f7015a8ccc540dba36f6b7696cc6ae9
Bushido008 <157196220+Bushido008@users.noreply.github.com>
2024-10-02 22:32:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if ($local_commit !== $remote_commit) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR Encryption</title>
+    <title>QR Code Encryption</title>
     <!-- Include Google Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
